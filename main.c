3f9a62dac3b71c72e5c31440c32c131a5e806468
Daka44 <jyp080404@gmail.com>
2025-04-09 12:21:35 +0900

    init

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    //절대로 다른 줄을 수정하지 마세요.
+    //자기 이름 옆에 아무 글을 써주세요.
+    printf("강상우 : --");
+    printf("고 윤 : --");
+    printf("김건우 : --");
+    printf("김주영 : --");
+    printf("금재준 : --");
+    printf("나하진 : --");
+    printf("노현우 : --");
+    printf("양서진 : --");
+    printf("윤준서 : --");
+    printf("장한울 : --");
+    printf("최희원 : --");
+    return 0;
+}
